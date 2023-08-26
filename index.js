d8e6968f3b3895659b8dbc787c2bddc3b8f8f08a
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-08-26 21:48:53 +0100

    completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addingEventListener() {
     const button = document.getElementById('button');
   
     button.addEventListener('click', function() {
-      console.log('Button clicked!');
+      alert('Button clicked!');
     });
   }
   
